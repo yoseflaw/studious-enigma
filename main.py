5652cb8ab96d44f38ef0b78882d0bf850bb43cff
yoseflaw <yosefardhitowin@gmail.com>
2024-01-05 18:04:05 +0700

    change iframe into html

--- main.py
+++ main.py
@@ -4,19 +4,29 @@ import os
 import jwt
 import time
 
-METABASE_SITE_URL = "https://telmark.metabaseapp.com"
+METABASE_SITE_URL = st.secrets["METABASE_SITE"]
 METABASE_SECRET_KEY = st.secrets["METABASE_SECRET"]
 
 payload = {
   "resource": {"dashboard": 1},
-  "params": {
-    
-  },
+  "params": {},
   "exp": round(time.time()) + (60 * 10) # 10 minute expiration
 }
 token = jwt.encode(payload, METABASE_SECRET_KEY, algorithm="HS256")
 
-iframeUrl = METABASE_SITE_URL + "/embed/dashboard/" + token + "#bordered=true&titled=true"
+iframeUrl = METABASE_SITE_URL + "/embed/dashboard/" + token + "bordered=true&titled=true"
 
 # embed streamlit docs in a streamlit app
-components.iframe(iframeUrl, width=1024, height=864, scrolling=True)
+st.set_page_config(layout="wide")
+# components.iframe(iframeUrl, width=300, height=800, scrolling=True)
+components.html(f"""
+                <iframe
+                  src="{iframeUrl}"
+                  frameborder="0"
+                  width="100%"
+                  height="600"
+                  allowtransparency
+              ></iframe>
+                """,
+                height=600,
+                scrolling=True)
